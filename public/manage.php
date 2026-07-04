1a75ade66acfa73d01fdbf9fd800470fefa0fb65
Wycliffe Omondi Otieno <wycliffomondiotieno@gmail.com>
2026-07-04 22:47:13 +0300

    Added a saqle install command for project initialization

--- public/manage.php
+++
@@ -1,7 +0,0 @@
-<?php
-require dirname(__DIR__).'/vendor/autoload.php';
-require dirname(__DIR__).'/autoload.php';
-
-$app_factory = require dirname(__DIR__).'/bootstrap/app.php';
-$app = $app_factory();
-$app->run_cli($argv);
