4b358fb384b065a4f639cb2cec2613db64999679
Wycliffe Omondi Otieno <wycliffomondiotieno@gmail.com>
2026-07-06 10:14:59 +0300

    Added a config setting for etra routes discovery

--- config/app.php
+++ config/app.php
@@ -82,6 +82,15 @@ return [
      'extra_models_dirs' => [],
 
      /**
+       * By default, routes will be searched in the top level project directory, or inside
+       * the individual module directories as listed in modules. 
+       * 
+       * In cases where your routes also exist in other places, list the directory
+       * names here relative to the root directory.
+     * */
+     'extra_routes_dirs' => [],
+
+     /**
        * This is the extension of component temnplate files:
        * 
        * Defaults to html
